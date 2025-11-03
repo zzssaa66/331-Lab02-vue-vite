e007e420711130bbd34a45d8168f9c1b6352b439
Zhou Sian <18680652603@163.com>
2025-11-03 10:18:16 +0800

    5

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+inject()
 
 app.mount('#app')
